e55ab74af2089330767def52956e1ae5e539bac7
Iman Tabrizian <Tabrizian@outlook.com>
2017-09-04 00:54:20 +0430

    Improves webpack significantly but not working

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,32 @@
 const path = require('path');
 
 module.exports = {
-  entry: './lib/',
+  entry: [
+    'babel-polyfill',
+    './lib/'
+  ],
   output: {
     filename: 'mqttsocket.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['es2015'],
+              plugins:[
+                'transform-runtime'
+              ],
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'source-map'
 };
